f4ad1682083e12ea6b910cef00619b11609b5a49
liumin1128 <970568830@qq.com>
2018-08-28 18:49:15 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { PureComponent, Fragment } from 'react';
+import ReactPlayer from 'react-player'
+import { Player } from 'video-react';
 import logo from './logo.svg';
 import { loadHeadFile } from './utils'
-
+import "video-react/dist/video-react.css"; // import css
 class App extends PureComponent {
   componentDidMount() {
     this.loadQiniuPlayer()
@@ -27,6 +29,12 @@ class App extends PureComponent {
     return (
       <Fragment>
         <video id="demo-video" className="video-js vjs-big-play-centered"></video>
+        <hr/>
+        <ReactPlayer url='https://imgs.huarenhouse.com/APP%E4%BB%8B%E7%BB%8DV3.mov' />
+        <hr/>
+        <Player>
+          <source src="https://imgs.huarenhouse.com/APP%E4%BB%8B%E7%BB%8DV3.mov" />
+        </Player>
       </Fragment>
     );
   }
